816e0d5cee85e206118cda2974c21a9bfe820ca8
Colby Wang <colby.wang765@gmail.com>
2022-02-08 22:32:32 -0500

    Finished main gameplay loop, need to add aces being able to count as 11 or 1 in the score calc

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ struct Deck {
     int cardsLeft;
 };
 
+
 struct Player {
     int totalHandValue;
     int cards[16];
@@ -15,32 +16,8 @@ struct Player {
     int cardsInHand;
 };
 
-struct Dealer {
-    int totalHandValue;
-    int cards[16];
-    int cardIndex;
-    int cardsInHand;
-};
 
-
-void dealDealer(struct Dealer* deal, struct Deck* d) {
-    int deckIndex = (rand()%d->cardsLeft+1);
-    //Checks if the index in the array is empty, if so rerolls
-    while(d->cards[deckIndex] == 0) {
-        deckIndex = (rand()%d->cardsLeft+1);
-    }
-    //Adds the card from the deck into the player hand
-    deal->cards[deal->cardIndex] = d->cards[deckIndex];
-    deal->totalHandValue += deal->cards[deal->cardIndex];
-    //Sets the card in deck array to 0
-    d->cards[deckIndex] = 0;
-    d->cardsLeft -= 1;
-    //Increments the card index and cards in hand for the player
-    deal->cardIndex += 1;
-    deal->cardsInHand += 1;
-}
-
-void dealPlayer(struct Player* p, struct Deck* d) {
+void dealCards(struct Player* p, struct Deck* d) {
     int deckIndex = (rand()%d->cardsLeft+1);
     //Checks if the index in the array is empty, if so rerolls
     while(d->cards[deckIndex] == 0) {
@@ -90,34 +67,105 @@ void initDeck(struct Deck* d) {
     }
 }
 
-void displayDeck(struct Deck* d) {
-    for(int i = 0; i < 52; i++) {
-        printf("%i, ",d->cards[i]);
+void initPlayer(struct Player* p) {
+    p->cardIndex = 0;
+    p->totalHandValue = 0;
+    p->cardsInHand = 0;
+}
+
+int dealMoreCards() {
+    printf("Would you like another card? (y/n): ");
+    char choice;
+    scanf(" %c", &choice);
+    if(choice == 'Y' || choice == 'y') {
+        return 1;
+    } else {
+        return 0;
     }
 }
+
+void displayHand(struct Player* p) {
+    printf("\nYou currently have: ");
+    for(int i = 0; i < p->cardsInHand; i++) {
+        printf("%i ",p->cards[i]);
+    }
+    printf("\n");
+}
+
+void evalWinner(struct Player* p, struct Player* d) {
+    printf("\nYour hand score is: %i\n", p->totalHandValue);
+    printf("\nThe dealers hand score is: %i\n", d->totalHandValue);
+    if(p->totalHandValue == 21) {
+        printf("Blackjack! You won.");
+        return;
+    } else if (d->totalHandValue == 21) {
+        printf("Blackjack! Dealer won.");
+        return;
+    }
+    if(p->totalHandValue > 21 && d->totalHandValue > 21){
+        printf("You both busted.");
+        return;
+    } else if(p->totalHandValue > 21) {
+        printf("Dealer won!");
+        return;
+    } else if (d->totalHandValue > 21) {
+        printf("You won, dealer busted!");
+        return;
+    }
+    if(p->totalHandValue < 21 && p->totalHandValue > d->totalHandValue) {
+        printf("You won!");
+        return;
+    } else {
+        printf("You lost!");
+        return;
+    }
+}
+
+
 
 void run() {
     printf("Welcome to Black Jack, would you like to play? (y/n)\n");
     char choice;
     scanf(" %c", &choice);
+    int running = 0;
     struct Player play;
-    play.cardIndex = 0;
-    play.cardsInHand = 0;
-    play.totalHandValue = 0;
-    struct Dealer deal;
-    deal.cardIndex = 0;
-    deal.cardsInHand = 0;
-    deal.totalHandValue = 0;
+    initPlayer(&play);
+    struct Player dealer;
+    initPlayer(&dealer);
     struct Deck currDeck;
     initDeck(&currDeck);
-    displayDeck(&currDeck);
-    if(choice == 'y') {
-        dealDealer(&deal,&currDeck);
-        dealPlayer(&play,&currDeck);
-    } else {
-        return;
+    if(choice == 'y' || choice == 'Y') {
+        running = 1;
+    };
+    while(running == 1) {
+        dealCards(&dealer, &currDeck);
+        dealCards(&play, &currDeck);
+        displayHand(&play);
+        int moreCards = dealMoreCards();
+        char hit;
+        if(moreCards == 1) {
+            hit = 'y';
+        }
+        while(dealer.totalHandValue < 17) {
+            dealCards(&dealer, &currDeck);
+        }
+        while(moreCards == 1) {
+            if (hit == 'y' || hit == 'Y') {
+                dealCards(&play, &currDeck);
+                displayHand(&play);
+            } else if (hit == 'n' || hit == 'N') {
+                moreCards = 0;
+            }
+            if(play.totalHandValue > 21) {
+                printf("You went above 21, you lost.");
+                moreCards = 0;
+                break;
+            }
+            moreCards = dealMoreCards();
+        }
+        running = 0;
     }
-
+    evalWinner(&play, &dealer);
 }
 
 int main() {
